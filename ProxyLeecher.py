c523e67505bba2b4909a0ff633a85c258adeb1b9
Barry Far <31126364+Bardiafa@users.noreply.github.com>
2023-06-06 22:41:55 +0330

    Added More Sources😍✅

--- ProxyLeecher.py
+++ ProxyLeecher.py
@@ -603,6 +603,21 @@ async def source45():
     except:
         return "s45:fail"
     
+async def source46():
+    global proxys
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt") as response:
+                lines = await response.text()
+                for line in lines.split("\n"):
+                    if line.strip():
+                        proxys.add(line.strip())
+        return "s46:ok"
+    except:
+        return "s46:fail"
+    
+    
+    
 async def main():
     global proxys
     with ThreadPoolExecutor(max_workers=20) as executor:
@@ -652,6 +667,7 @@ async def main():
         tasks.append(asyncio.ensure_future(source43()))
         tasks.append(asyncio.ensure_future(source44()))
         tasks.append(asyncio.ensure_future(source45()))
+        tasks.append(asyncio.ensure_future(source46()))
 
         # Wait for all tasks to complete before continuing
         results = await asyncio.gather(*tasks)
